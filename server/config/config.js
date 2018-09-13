d3d6034331c879148d8f1d02824f4e2769260715
Paul Hidalgo <paul.alex.hidalgo@gmail.com>
2018-09-13 11:02:32 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if(process.env.NODE_ENV==='dev'){
     urlDB='mongodb://localhost:27017/cafe';
 }else{
-    urlDB='mongodb://cafe-user:phidalgot06@ds253922.mlab.com:53922/cafe';
+    urlDB=process.env.MONGO_URI;
 }
 
 process.env.URLDB=urlDB;
