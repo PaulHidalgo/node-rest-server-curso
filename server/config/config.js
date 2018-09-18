04aa0fb1c3cef0bbec423be544050723f33dd6b1
Paul Hidalgo <paul.alex.hidalgo@gmail.com>
2018-09-18 11:56:28 -0500

    Login with google

--- server/config/config.js
+++ server/config/config.js
@@ -34,3 +34,8 @@ process.env.CADUCIDAD_TOKEN=60*60*24*30;
  * Seed de autenticación
  */
 process.env.SEED=process.env.SEED||'este-es-el-seed-desarrollo';
+
+/**
+ * Google CLient ID
+ */
+process.env.CLIENT_ID=process.env.CLIENT_ID||'742436936329-j2tfc1p5va2sq4sfah9t3e3ur4j5hshh.apps.googleusercontent.com'
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const bodyParser = require('body-parser');
 
@@ -14,6 +15,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+// hablitar la carpeta pública
+app.use(express.static(path.resolve( __dirname , '../public')))
 
 //COnfiguración global de rutas
 app.use(require('./controladores/index'));
